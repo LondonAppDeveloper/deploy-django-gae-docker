8c1ee1decaea9d8e11bcbb684406c0b2ac68dd30
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-27 12:01:37 +0100

    Added placeholder.

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from demo import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.demo),
 ]
--- app/demo/views.py
+++ app/demo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def demo(request):
+    return render(request, 'demo/demo.html')
